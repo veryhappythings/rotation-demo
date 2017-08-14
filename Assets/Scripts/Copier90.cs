0b985d0d34cfb8b21fdc1e3a74f01c8c573c2896
Mac Chapman <mac@veryhappythings.co.uk>
2017-08-14 12:41:09 +0100

    Tidier rounding to nearest 90 degrees

--- Assets/Scripts/Copier90.cs
+++ Assets/Scripts/Copier90.cs
@@ -13,17 +13,7 @@ public class Copier90 : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         Vector3 t = target.transform.rotation.eulerAngles;
-        if (t.z < 45) {
-            t.z = 0;
-        } else if (t.z < 135) {
-            t.z = 90;
-        } else if (t.z < 225) {
-            t.z = 180;
-        } else if (t.z  < 315) {
-            t.z = 270;
-        } else {
-            t.z = 0;
-        }
+        t.z = Mathf.Round(t.z / 90) * 90;
 
         transform.eulerAngles = new Vector3(180f, 0, t.z);
 	}
